206b65b51b18c485b59e78f8298c9a71c262a9d8
DrashtiSarvaiya <119496080+DrashtiSarvaiya@users.noreply.github.com>
2023-03-02 22:30:51 +0100

    File added

---
+++ ForLoop.py
@@ -0,0 +1,8 @@
+print("Count to 10!")
+
+#writing for loop
+#Python uses indentation to determine that the print statement is inside the for loop statement.
+
+for x in range (0, 11):
+    print(x)
+    
