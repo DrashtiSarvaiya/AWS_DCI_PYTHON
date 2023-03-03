ee69cda85bec91bbc7b3123254b879b4c79d003f
DrashtiSarvaiya <drashtiksarvaiya@gmail.com>
2023-03-03 16:10:43 +0100

    Demo: Conditionals(FlowControl)

---
+++ Conditionals.py
@@ -0,0 +1,11 @@
+x = input("How many Bananas do you have?:")
+Bananas = int(x)
+
+if Bananas >= 5 :
+    print("I have a bunch of Bananas")
+elif Bananas >= 1 and Bananas <= 5:
+    print("I have a small bunch of Bananas")
+else:
+     print("I have no bunch of Bananas")
+
+
