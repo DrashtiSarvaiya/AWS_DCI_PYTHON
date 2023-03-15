9d62b333d154a6aade4a10c7b12da9d3e2a63f32
Drashti Sarvaiya <119496080+DrashtiSarvaiya@users.noreply.github.com>
2023-03-15 11:20:47 +0100

    Delete ForLoop.py

--- ForLoop.py
+++
@@ -1,8 +0,0 @@
-print("Count to 10!")
-
-#writing for loop
-#Python uses indentation to determine that the print statement is inside the for loop statement.
-
-for x in range (0, 11):
-    print(x)
-    
